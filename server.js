db07176774a9750488033d38cbf35451233d5347
Rishi8603 <rishii8603@gmail.com>
2025-09-04 09:55:44 +0530

    change1

--- server.js
+++ server.js
@@ -59,6 +59,11 @@ app.put('/api/tasks/:id', (req, res) => {
   res.json(tasks[taskIndex]);
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
