4f9f7ae0f5e8ac08d38ecfb42865558adc33e826
Rishi8603 <rishii8603@gmail.com>
2025-09-13 18:43:49 +0530

    heading change

--- frontend/src/Components/ui/Header.jsx
+++ frontend/src/Components/ui/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
     <>
       <div className="fixed top-0 left-0 w-full h-16 bg-zinc-900 border-b border-zinc-700 
       flex items-center justify-between px-6 z-50">
-        <h1 className="text-white text-lg font-bold">Todo</h1>
+        <h1 className="text-white text-lg font-bold">Taskit</h1>
 
         <button
           className="lg:hidden text-white text-2xl"
